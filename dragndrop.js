618debcc6830fcf0dcfc8ba13f7b5dbc377ce559
Xuthus2022 <readjethro@gmail.com>
2022-05-25 00:45:07 +1200

    advancements with dragndrop functions - works similar to trello

--- dragndrop.js
+++ dragndrop.js
@@ -1,66 +1,108 @@
-const list_items = document.querySelectorAll('.list-item');
+const listItems = document.querySelectorAll('.list-item');
 const lists = document.querySelectorAll('.list');
 const clicky = document.querySelector('#da-button');
-
+const middleList = document.getElementById("2");
 let draggedItem = null;
 
-const dragItUpFor = (item) => {
+const cleanUpItemDragging = (item) => {
 	item.addEventListener('dragstart', function () {
-		console.log("Start");
 		draggedItem = item;
-		setTimeout(function () {
-			item.style.display = 'none';
+		setTimeout(function () {	// tiny delay needed I don't know why
+			item.style.visibility = 'hidden';
 		}, 0);
 	});
 
 	item.addEventListener('dragend', function () {
-		console.log("End");
-		setTimeout(function () {
-			draggedItem.style.display = 'block';
+		draggedItem.style.visibility = 'initial'
+		draggedItem.classList.remove("selected");
+		setTimeout(function () {	// tiny delay needed I don't know why
 			draggedItem = null;
-		}, 0)
+		}, 0);
 	});
 };
- 
-// initial setup
-for (let i = 0; i < list_items.length; i++) {
-	const item = list_items[i];
 
-	dragItUpFor(item);
+const itemsFor = (list) => {
+	return [...list.querySelectorAll('.list-item')];
+};
 
+const getYPositionOfCenter = (e) => {
+	const rect = e.getBoundingClientRect();
+	return (rect.top + (rect.height/2));
+}
 
-	for (let j = 0; j < lists.length; j ++){
+const cleanupDragForLists = () => {
+	for (let j = 0; j < lists.length; j++){
 		const list = lists[j]; 
-
+	
 		list.addEventListener('dragover', function (e) {
 			e.preventDefault();
 		})
-		list.addEventListener('drapenter', function(e) {
+		list.addEventListener('dragenter', function(e) {
 			e.preventDefault(); 
-			this.backgroundColor = 'rgba(0, 0, 0, 0.2)';
+			this.style.backgroundColor = 'rgba(0, 0, 0, 0.2)';
 
 		})
 		list.addEventListener('dragleave', function (e) {
 			e.preventDefault();
-			this.backgroundColor = 'rgba(0, 0, 0, 0.1)';
-
+			this.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
+	
 		})
 		list.addEventListener('drop', function (e) {
-			this.append(draggedItem);
-			this.backgroundColor = 'rgba(0, 0, 0, 0.1)';
+			mouseY = e.clientY;
+			const listChildren = itemsFor(list);
+			let firstItemLowerThanMouse;
 
+			if(listChildren.length === 0) { 
+				list.append(draggedItem);
+			} else {
+				firstItemLowerThanMouse = listChildren.find(item => (getYPositionOfCenter(item) - mouseY) >= 0);
+				if(firstItemLowerThanMouse !== null) {
+					console.log("YEY");
+					// debugger;
+					list.insertBefore(draggedItem, firstItemLowerThanMouse);
+				} else {
+					list.append(draggedItem);
+				}
+			}
+			
+			this.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
 		})
 	}
-} 
+};
+
+const makeSelectable = (item) => {
+	item.addEventListener('click', function () {
+		item.classList.toggle("selected");
+	});
+}
 
 
-clicky.addEventListener('click', function (e) {
-	e.preventDefault();
-	console.log("gidday");
+const setupDragAndDrop = () => {
+	cleanupDragForLists();
+
+	for (let i = 0; i < listItems.length; i++) {
+		const item = listItems[i];
+		cleanUpItemDragging(item);
+		makeSelectable(item);
+	} 
+};
+
+setupDragAndDrop();
+
+
+
+
+const createListItem = () => {
 	const element = document.createElement('div');
 	element.setAttribute("class", "list-item");
 	element.setAttribute("draggable", "true");
 	element.textContent = "REEEEEEEEEEEE";
-	dragItUpFor(element);
-	document.getElementById("2").appendChild(element);
+	cleanUpItemDragging(element);
+	makeSelectable(element);
+	middleList.appendChild(element);
+}
+
+clicky.addEventListener('click', function (e) {
+	e.preventDefault();
+	createListItem();
 })
